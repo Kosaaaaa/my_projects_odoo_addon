1970284dcf2ceb307d49d1d4adc3767fc5fd0e1b
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-29 17:32:40 +0100

    add Task and Project model

--- models/models.py
+++ models/models.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+
+class Project(models.Model):
+    _name = 'my_projects.project'
+    _description = 'My Projects Project Model'
+
+    name = fields.Char(string='Project Name', required=True)
+    description = fields.Text()
+    created_date = fields.Date(default=lambda self: fields.Date.today(), readonly=True)
 
 
-# class my_projects(models.Model):
-#     _name = 'my_projects.my_projects'
-#     _description = 'my_projects.my_projects'
+class Task(models.Model):
+    _name = 'my_projects.task'
+    _description = 'My Projects Task Model'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Task name', required=True)
+    description = fields.Text()
+    created_date = fields.Date(default=lambda self: fields.Date.today(), readonly=True)
+    due_date = fields.Date(required=True)
+    is_completed = fields.Boolean(default=False, string='Completed')
