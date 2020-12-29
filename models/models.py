7b557729423cecf824f1bb27595b136cc2ee655c
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2020-12-29 18:48:37 +0100

    add project_id field with Many2one relation to Task model

--- models/models.py
+++ models/models.py
@@ -10,7 +10,6 @@ class Project(models.Model):
     description = fields.Text()
     created_date = fields.Date(default=lambda self: fields.Date.today(), readonly=True)
 
-
 class Task(models.Model):
     _name = 'my_projects.task'
     _description = 'My Projects Task Model'
@@ -20,3 +19,4 @@ class Task(models.Model):
     created_date = fields.Date(default=lambda self: fields.Date.today(), readonly=True)
     due_date = fields.Date(required=True)
     is_completed = fields.Boolean(default=False, string='Completed')
+    project_id = fields.Many2one('my_projects.project', ondelete='cascade', string='Project')
